d313abe51ae7e372f876e773ed5fae4b4411826d
Jack Toke <jack.toke@rhythmix.com.au>
2025-07-17 01:51:28 +0000

    Refactored customer_order_history to be simpler

--- dbt_analytics/models/staging/stg_orders.sql
+++ dbt_analytics/models/staging/stg_orders.sql
@@ -1,8 +1,24 @@
-SELECT 
+WITH order_source AS (
+    SELECT 
+        id,
+        user_id,
+        order_date,
+        status
+    FROM
+        {{ source('order_gcp_source', 'orders') }}
+    WHERE status != 'pending'
+)
+SELECT
     id AS order_id,
-    user_id,
+    user_id AS customer_id,
     order_date,
-    status AS order_status
-FROM
-    {{ source('order_gcp_source', 'orders') }}
-WHERE status != 'pending'
+    status AS order_status,
+    CASE 
+        WHEN status NOT IN ('returned', 'return_pending')
+        THEN order_date
+    END AS valid_order_date,
+    ROW_NUMBER() OVER (
+        PARTITION BY user_id
+        ORDER BY order_date, id
+    ) AS user_order_seq
+FROM order_source
