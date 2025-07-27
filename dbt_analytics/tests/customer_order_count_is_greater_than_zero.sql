aa4d2798f0c0d9b9ea9bb1dec5305dba05a1b402
Jack Toke <jack.toke@rhythmix.com.au>
2025-07-27 12:57:57 +0000

    Added singular test on customer_order_history

---
+++ dbt_analytics/tests/customer_order_count_is_greater_than_zero.sql
@@ -0,0 +1,6 @@
+SELECT 
+    order_count
+FROM {{ ref('customer_order_history') }}
+WHERE order_count <= 0
+-- The condition should be the opposite of what you are testing
+-- An error will be thrown when the result is not empty
